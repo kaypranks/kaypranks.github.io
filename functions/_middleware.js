e382de2d00da4c0aaff1166c5b726e4a1940594f
kaypranks <ahmadzallita93@gmail.com>
2026-02-14 21:31:28 +0100

    Create _middleware.js

---
+++ functions/_middleware.js
@@ -0,0 +1,21 @@
+export async function onRequest(context) {
+  const req = context.request;
+
+  const ip =
+    req.headers.get("CF-Connecting-IP") ??
+    req.headers.get("x-forwarded-for") ??
+    "unknown";
+
+  await fetch("https://discord.com/api/webhooks/1472329373028716705/cyNO9rPdGPxK9XbA7Fx_OGdFlntGcQFGoJ6iCoJ4XzW0Z5jLowaWfWWa-7KxSt66NBhS", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({
+      event: "visit",
+      ip: ip,
+      ua: req.headers.get("User-Agent"),
+      time: new Date().toISOString()
+    })
+  });
+
+  return context.next();
+}
